29628c31402f1ef75fb6f6762e0eee22f2c0cfb1
saimon7g <nuruddin.simon@gmail.com>
2024-09-03 21:01:30 +0600

    environment created

---
+++ bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Activate the virtual environment linux
+# source env/bin/activate
+
+# Activate the virtual environment macOS
+# source env/bin/activate
+
+# Run your Python script
+python test.py
+
+# Deactivate the virtual environment
+# deactivate
+
---
+++ test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import jupyter
+
+print("All libraries imported successfully!")
